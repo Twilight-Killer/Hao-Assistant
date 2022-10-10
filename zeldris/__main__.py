c8121df0a2133b7ce4cb4e4088ae53ecf3d242eb
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2022-10-10 16:55:59 +0700

    Update __main__.py

--- zeldris/__main__.py
+++ zeldris/__main__.py
@@ -1,6 +1,6 @@
-# ZeldrisRobot
+# Haotogel-Official
 # Copyright (C) 2017-2019, Paul Larsen
-# Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
+# Copyright (C) 2022, HaoTogelLivedraw Team, <https://github.com/Twilight-Killer/Hao-Assistant>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -93,42 +93,42 @@ def get_readable_time(seconds: int) -> str:
 
 
 # sourcery skip: raise-specific-error
-ZELDRIS_IMG = "https://telegra.ph/file/1fa00785f30375c0c1b50.jpg"
+ZELDRIS_IMG = "https://te.legra.ph/file/176f7c4d641bf9dfaf2bd.jpg"
 
 PM_START_TEXT = """
-Hey there! my name is *{}*. 
-A modular group management bot with useful features. [ㅤ](https://telegra.ph/file/fed9ba09e9add9b197c21.png)
+Hei! nama saya adalah *{}*. 
+Bot manajemen grup modular dengan fitur berguna. [ㅤ](https://te.legra.ph/file/176f7c4d641bf9dfaf2bd.jpg)
 
 ◑ *Uptime:* `{}`
 ◑ `{}` *Users, across* `{}` *chats.*
 
-Any issues or need help related to me?
-Join our official group [IDNCoderX](https://t.me/IDNCoderX).
+Apakah kamu ingin menggunakan nya?
+Jika ingin menggunakan nya hubungi owner [DarkiezZzz](https://t.me/DarkiezZzz).
 Click help button to know my commands!
 """
 
 buttons = [
     [
         InlineKeyboardButton(
-            text="❔ Help",
+            text="Help",
             callback_data="help_back",
         ),
         InlineKeyboardButton(
-            text="Updates 📢",
-            url="https://t.me/IDNCoder",
+            text="Group",
+            url="https://t.me/HaoTogelLivedraw",
         ),
     ],
     [
         InlineKeyboardButton(
-            text="Add Zeldris to Your Group 👥",
+            text="Tambahkan ke group",
             url="t.me/ZeldrisRobot?startgroup=true",
         ),
     ],
 ]
 
 HELP_STRINGS = f"""
-Hello there! My name is *{dispatcher.bot.first_name}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
+I'm *{dispatcher.bot.first_name}*.
+Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
 × /start: Starts me, can be used to check i'm alive or no...
@@ -266,12 +266,12 @@ def start(update: Update, context: CallbackContext):
                 [
                     [
                         InlineKeyboardButton(
-                            text="☎️ Support",
-                            url="https://t.me/IDNCoderX",
+                            text="Support",
+                            url="https://t.me/DarkiezZzz",
                         ),
                         InlineKeyboardButton(
-                            text="Updates 📡",
-                            url="https://t.me/IDNCoder",
+                            text="Group",
+                            url="https://t.me/HaoTogelLivedraw",
                         ),
                     ]
                 ]
@@ -392,7 +392,7 @@ def get_help(update: Update, context: CallbackContext):
         if len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
             module = args[1].lower()
             update.effective_message.reply_text(
-                f"Contact me in PM to get help of {module.capitalize()}",
+                f"Hubungi saya di PM untuk mendapatkan bantuan {module.capitalize()}",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
@@ -407,7 +407,7 @@ def get_help(update: Update, context: CallbackContext):
 
             return
         update.effective_message.reply_text(
-            "Contact me in PM to get the list of possible commands.",
+            "Hubungi saya di PM untuk mendapatkan daftar perintah.",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
